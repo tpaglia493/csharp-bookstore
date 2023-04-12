cc11032c27f89a9ff9979dc36a4d1795aaaed843
tpaglia493 <tommasopaglia93@outlook.it>
2023-04-12 14:57:28 +0200

    setup - variables declaration

--- BookStoreCSharp/Program.cs
+++ BookStoreCSharp/Program.cs
@@ -1,2 +1,15 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+
+//VARIABLES DECLARATION
+string Title = "Titolo";
+string Author = "NomeAutore";
+long ISBN = 4234567891;
+uint NumberOfPages = 1000;
+uint BookWidth = 25; //centimeters
+uint BookHeight = 40; //centimeters
+uint BookDepth = 4; //centimeters
+float BookRating = 4.5f;
+uint NumberOfReviews = 23453;
+bool IsKindleAvailable = false;
+bool IsFlexibleCoverAvailable = true;
